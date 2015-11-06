4382c49b0aabbb63b973dc0e17616c86d4dc613d
niC00L147 <nika.blanarova@gmail.com>
2015-11-06 09:55:52 +0100

    Added answer counter

--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -8,13 +8,13 @@ use Nette,
 class TestPresenter extends BasePresenter {
 
 	/** @var array */
-	private $allItems;
+	private $quest;
 	private $id;
 	private $options;
 
 	public function handleQuestion($id) {
 		$this->id = $id+1;
-		$this->allItems = $this->db->table('animals')->where('id', $this->id )->fetchAll();
+		$this->quest = $this->db->table('animals')->where('id', $this->id )->fetchAll();
 		$this->options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
 		if ($this->isAjax()) {
 			$this->redrawControl('ajaxChange');
@@ -25,14 +25,14 @@ class TestPresenter extends BasePresenter {
 		if ($this->id === NULL) {
 			$this->id = '1';
 		}
-		if ($this->allItems === NULL) {
-			$this->allItems = $this->db->table('animals')->where('id', $this->id)->fetchAll();
+		if ($this->quest === NULL) {
+			$this->quest = $this->db->table('animals')->where('id', $this->id)->fetchAll();
 		}
 		if($this->options === NULL) {
 			$this->options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
 		}
 		$this->template->options = $this->options;
-		$this->template->allItems = $this->allItems;
+		$this->template->quest = $this->quest;
 		$this->template->id = $this->id;
 	}
 
