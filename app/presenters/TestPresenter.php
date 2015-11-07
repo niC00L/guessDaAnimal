b5bb809843431e2f39e5685b873a3da65598778a
niC00L147 <nika.blanarova@gmail.com>
2015-11-08 00:08:54 +0100

    Edited ajax, added starting button

--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -3,40 +3,49 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
+    App\Model;
 
 class TestPresenter extends BasePresenter {
 
-	/** @var array */
-	private $quest;
-	private $id;
-	private $options;
+    /** @var array */
+    private $quest;
+    private $id;
+    private $options;
+    private $ajax;
 
-	public function handleQuestion($id) {
-		$this->id = $id+1;
-		$this->quest = $this->db->table('animals')->where('id', $this->id )->fetchAll();
-		$this->options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
-		if ($this->isAjax()) {
-			$this->redrawControl('ajaxChange');
-		}
-	}
+    public function handleQuestion() {
+        $this->quest = $this->db->table('animals')->order('RAND()')->limit(1)->fetchAll();
+        foreach ($this->quest as $a) {
+            $this->id = $a['id'];
+        }
+        $options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
+        $this->options = array_merge($this->quest, $options);
+        shuffle($this->options);
+        if ($this->isAjax()) {
+            $this->redrawControl('ajaxChange');
+        }
+        $this->ajax = $this->isAjax();
+    }
 
-	public function renderAnimals() {
-		if ($this->id === NULL) {
-			$this->id = '1';
-		}
-		if ($this->quest === NULL) {
-			$this->quest = $this->db->table('animals')->where('id', $this->id)->fetchAll();
-		}
-		if($this->options === NULL) {
-			$this->options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
-		}
-		$this->template->options = $this->options;
-		$this->template->quest = $this->quest;
-		$this->template->id = $this->id;
-	}
+    public function renderAnimals() {
+//        if ($this->quest === NULL) {
+//            $this->quest = $this->db->table('animals')->order('RAND()')->limit(1)->fetchAll();
+//            foreach ($this->quest as $a) {
+//                $this->id = $a['id'];
+//            }
+//        }
+//        if ($this->options === NULL) {
+//            $options = $this->db->table('animals')->limit(3)->order('RAND()')->where('NOT id', $this->id)->fetchAll();
+//            $this->options = array_merge($this->quest, $options);
+//            shuffle($this->options);
+//        }
+        $this->template->options = $this->options;
+        $this->template->quest = $this->quest;
+        $this->template->ajax = $this->ajax;
+    }
 
-	public function actionShowResults() {
-		
-	}
+    public function actionShowResults() {
+        
+    }
+
 }
