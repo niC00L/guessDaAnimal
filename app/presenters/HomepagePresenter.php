ed6220ddfc90f1d3f7a45a811744c834f5410a9b
niC00L147 <nika.blanarova@gmail.com>
2015-11-15 23:09:31 +0100

    Added preloading, edited templates

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,5 +5,7 @@ namespace App\Presenters;
 use Nette;
 
 class HomepagePresenter extends BasePresenter {
-	
+    public function renderDefault() {
+        $this->template->imgs = $this->db->table('animals')->select('img, img_black')->fetchAll();        
+    }
 }
